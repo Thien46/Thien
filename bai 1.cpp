29633667c4e3621e6853507a24a96683f04282c7
Thien46 <tranvuthien0406@gmail.com>
2019-10-05 03:31:28 -0700

    bai 1

---
+++ bai 1.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,max,min;
+	max=a;
+	min=a;
+	if (max<b){
+		max=b;
+	}
+	if (max<c){
+		max=c;
+	}
+	if (min>b){
+		min=b
+	}
+	if (min>c){
+		min=c
+	}
+	return 0;
+}
