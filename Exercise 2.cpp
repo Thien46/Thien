99b560c7e813e2e6354e876501630d0f8fef58a3
Thien46 <56116680+Thien46@users.noreply.github.com>
2019-10-31 19:50:50 +0700

    Create Exercise 2.cpp

---
+++ Exercise 2.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	float capital;
+	float interest;
+	float interestrate;
+	int years;
+	printf("Enter Capital Sum\n");
+	scanf("%f",&capital);
+	printf("Enter Rate of Interest\n");
+	scanf("%f",&interestrate);
+	printf("Enter No of years\n");
+	scanf("%d",&years);
+	for(int i=1;i<=years;i++){
+		interest=capital*(interestrate/100);
+		capital+=interest;
+		interest=capital;
+		printf("Year     Interest     Sum\n%d     %f     %f\n",i,interest,capital);
+	}
+	return 0;
+}
